eddcb0b33f71c3f82ad5e69c28b50ad8f31c3c3c
Nikolay Gromov <nicothin@gmail.com>
2018-10-30 16:23:49 +0300

    Добавил учебный компонент

---
+++ src/components/Lesson.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Lesson extends Component {
+  render() {
+    return <h1>Hell o!</h1>
+  }
+}
+
+export default Lesson;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Lesson from './components/Lesson.js';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const hrader = <h1>Хелл о ворлд</h1>;
+
+ReactDOM.render(<Lesson />, document.getElementById('root'));
 registerServiceWorker();
